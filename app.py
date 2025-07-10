eee2e38006ee69e368bdcd998dd4d1122e5263f6
Prachi Vedant <prachivedant2005@gmail.com>
2025-07-10 22:03:23 +0530

    added

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def predictRoute():
 
 if __name__ == "__main__":
     clapp = ClientApp()
-    app.run(host = '0.0.0.0',port = 8080)
+    app.run(host = '0.0.0.0',port = 80) # FOR AWS PORT NO 8080
